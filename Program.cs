298ca00b0f66382f4fb0b880cf48e8b17a3be0a7
mohsentux <mohsentux@live.com>
2022-02-16 03:26:29 +0330

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Set up Console UI (CUI)
+Console.Title = "My Rocking App";
+ConsoleColor defFore = Console.ForegroundColor;
+ConsoleColor defBack = Console.BackgroundColor;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("*************************************");
+Console.WriteLine("***** Welcome to My Rocking App *****");
+Console.WriteLine("*************************************");
+Console.ForegroundColor = defFore;
+Console.BackgroundColor = defBack;
