892402edcbbe62d1b4659fcffa83d2f4857e89c2
mohsentux <mohsentux@live.com>
2022-02-16 04:19:21 +0330

    Updates

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿// Set up Console UI (CUI)
-Console.Title = "My Rocking App";
-ConsoleColor defFore = Console.ForegroundColor;
-ConsoleColor defBack = Console.BackgroundColor;
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.BackgroundColor = ConsoleColor.Blue;
-Console.WriteLine("*************************************");
-Console.WriteLine("***** Welcome to My Rocking App *****");
-Console.WriteLine("*************************************");
-Console.ForegroundColor = defFore;
-Console.BackgroundColor = defBack;
+namespace SimpleCSharpApp {
+	class Program {
+		static void Main(string[] args) {
+			Console.Title = "My Rocking App";
+			ConsoleColor defFore = Console.ForegroundColor;
+			ConsoleColor defBack = Console.BackgroundColor;
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.BackgroundColor = ConsoleColor.Blue;
+			Console.WriteLine("*************************************");
+			Console.WriteLine("***** Welcome to My Rocking App *****");
+			Console.WriteLine("*************************************");
+			Console.ForegroundColor = defFore;
+			Console.BackgroundColor = defBack;
+		}
+	}
+}
